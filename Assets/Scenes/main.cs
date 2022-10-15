28fb48dbc357f96a78a27a60947ac7a00d48e643
lekenneth77 <lekenneth77@gmail.com>
2022-10-15 16:09:40 -0500

    Added multiple player movement
    multiple player movement is working, need to finish the rest of the squares, only 6 is completed

--- Assets/Scenes/main.cs
+++ Assets/Scenes/main.cs
@@ -7,11 +7,9 @@ using UnityEngine.SceneManagement;
 public class main : MonoBehaviour
 {
 
-    /*
+    /* Online References
       2D Dice Roll:
         https://www.youtube.com/watch?v=JgbJZdXDNtg&ab_channel=AlexanderZotov
-
-    
     */
 
     //Instance Variables
@@ -25,27 +23,73 @@ public class main : MonoBehaviour
     // Reference to sprite renderer to change sprites
     private Image rend;
 
+    private BoardSpace[] board = {
+        new BoardSpace(220, -112, 112, 92),
+        new BoardSpace(133, -112, 88, 92),
+        new BoardSpace(44, -112, 115.5, 92),
+        new BoardSpace(-43, -112, 105, 92),
+        new BoardSpace(-127, -112, 85, 92),
+        new BoardSpace(-220, -112, 142, 92),
+        new BoardSpace(-241, -17, 93.5, 132)
+    };
+
+    private Player[] order = new Player[4];
+    private int curr_turn = 0;
     private Player player_one;
+    private Player player_two;
+    private Player player_three;
+    private Player player_four;
+
     private int roll;
 
     //Classes
     class Player {
-        public int cur_pos = 0;
-        public string name;
-        //public Sprite/Image character;
         public GameObject token;
+        public string name;
+        public int id;
 
-         public Player(string player_name, GameObject player_token) {
+        public int curr_pos = 0;        
+        public bool reverse_path = false;
+
+         public Player(string player_name, int id, Canvas canvas) {
             name = player_name;
-            token = player_token;
+            this.id = id;
+
+            //create token
+            GameObject tok = new GameObject("player_" + id + "_token");
+            //handle sizing and position
+            RectTransform trans = tok.AddComponent<RectTransform>();
+            trans.transform.SetParent(canvas.transform);
+            //move position
+            trans.localScale = Vector3.one;
+            //size of token
+            trans.sizeDelta = new Vector2(25, 25);
+            //handle image
+            Image image = tok.AddComponent<Image>();
+            Texture2D tex = Resources.Load<Texture2D>("coin_" + id);
+            image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+            tok.transform.SetParent(canvas.transform);
+            token = tok;
         }
     }
 
-    //make an array for the boardgame 
-    //
+    class BoardSpace {
+        public float center_x;
+        public float center_y;
+        public double width;
+        public double height;
+        public Queue<Player> players_on_me = new Queue<Player>();
+        public bool lose_a_turn = false;
+        //add which transition scene to use?
 
+        public BoardSpace(float x, float y, double w, double h) {
+            center_x = x;
+            center_y = y;
+            width = w;
+            height = h;
+        }
+    }
    
-
 
     // Start is called before the first frame update
     void Start() {
@@ -62,29 +106,54 @@ public class main : MonoBehaviour
         diceSides = Resources.LoadAll<Sprite>("Dice/");
         rend.sprite = diceSides[0]; 
 
-        //Create Player_One token
-        GameObject token = new GameObject("player_token");
-        //handle sizing and position
-        RectTransform trans = token.AddComponent<RectTransform>();
-        trans.transform.SetParent(canvas.transform);
-        //move position
-        trans.localPosition = new Vector2(220, -110);
-        trans.localScale = Vector3.one;
-        //size of token
-        trans.sizeDelta = new Vector2(37, 37);
-        //handle image
-        Image image = token.AddComponent<Image>();
-        Texture2D tex = Resources.Load<Texture2D>("coin");
-        image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-        token.transform.SetParent(canvas.transform);
-        
-        player_one = new Player("test_name", token);
+        //TODO temporary, take this out
+        player_one = new Player("test_name", 1, canvas);
+        player_two = new Player("test_name", 2, canvas);
+        player_three = new Player("test_name", 3, canvas);
+        player_four = new Player("test_name", 4, canvas);
+        BoardSpace curr_square = board[0];
+
+        //TODO ADD RANDOMIZE ORDER LATER
+        order[0] = player_one;
+        order[1] = player_two;
+        order[2] = player_three;
+        order[3] = player_four;
+
+        //TODO YOU NEED TO ENQUEUE IN THE CORRECT RANDOMIZED ORDER AT THE START OF THE GAME FOR THE LOGIC TO WORK
+        curr_square.players_on_me.Enqueue(player_one);
+        curr_square.players_on_me.Enqueue(player_two);
+        curr_square.players_on_me.Enqueue(player_three);
+        curr_square.players_on_me.Enqueue(player_four);
+        update_player_pos(curr_square);
         
     }
+
 
     // Update is called once per frame
     void Update() {
         
+    }
+
+    /*
+        Given a board space, update the positions of every player on that board space.
+    */
+    void update_player_pos(BoardSpace curr_square) {
+        //TODO really gacky try and fix this
+        Queue<Player> player_q = curr_square.players_on_me;
+        int pos_count = 0;
+        foreach (Player player in player_q) {
+            GameObject player_token = player.token;
+            if (pos_count == 0) {
+                player_token.GetComponent<RectTransform>().localPosition = new Vector2(curr_square.center_x - (float) (curr_square.width / 5) , curr_square.center_y + (float) (curr_square.height / 5));
+            } else if (pos_count == 1) {
+                player_token.GetComponent<RectTransform>().localPosition = new Vector2(curr_square.center_x + (float) (curr_square.width / 5) , curr_square.center_y + (float) (curr_square.height / 5));
+            } else if (pos_count == 2) {
+                player_token.GetComponent<RectTransform>().localPosition = new Vector2(curr_square.center_x - (float) (curr_square.width / 5) , curr_square.center_y - (float) (curr_square.height / 5));
+            } else if (pos_count == 3) {
+                 player_token.GetComponent<RectTransform>().localPosition = new Vector2(curr_square.center_x + (float) (curr_square.width / 5) , curr_square.center_y - (float) (curr_square.height / 5));
+            }
+            pos_count++;
+        }
     }
 
       private void click_dice()
@@ -123,34 +192,27 @@ public class main : MonoBehaviour
         Debug.Log("Dice Roll: " + roll);
 
         //dynamic player movement
-        // RectTransform trans = player_one.token.GetComponent<RectTransform>();
-        // trans.localPosition = new Vector2(trans.localPosition.x - (75 * roll), -110);
-        move_pos(player_one, roll);
+        Player curr_player = order[curr_turn];
+        curr_turn++;
+        if (curr_turn >= 4) {
+            curr_turn = 0;
+        }
+        move_pos(curr_player, roll);
     }
 
      void move_pos(Player player, int move) {
-        player.cur_pos += move; //TODO don't forget this
-        Debug.Log("Player's Current Position: " + player.cur_pos);
-        int x = 0;
-        switch(player.cur_pos) {
-            case 1:
-                x = 135;
-                SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
-                break;
-            case 2:
-                x = 45; break;
-            case 3:
-                x = -46; break;
-            case 4: 
-                x = -126; break;
-            case 5:
-                x = -222; break;
-            default:
-                x = 220;
-                player.cur_pos = 0;
-                break;
-        }
-        player_one.token.GetComponent<RectTransform>().localPosition = new Vector2(x, -110);
+        board[player.curr_pos].players_on_me.Dequeue();
+        //TODO uncomment the line below if you want to update the space before moving as well
+        // update_player_pos(board[player.curr_pos]);
+        player.curr_pos = player.reverse_path ? player.curr_pos -= move : player.curr_pos += move;
+        Debug.Log("Player's Current Position On Board: " + (player.curr_pos + 1));
+        BoardSpace curr_square = board[player.curr_pos];
+        curr_square.players_on_me.Enqueue(player);
+        update_player_pos(board[player.curr_pos]);
+
+        // SceneManager.LoadSceneAsync(player.curr_pos + 1, LoadSceneMode.Additive);
+        // SceneManager.LoadSceneAsync(player.curr_pos + 1, LoadSceneMode.Single);
+ 
 
     }
 
