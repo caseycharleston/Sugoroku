7ac2ada976c212c350097443b211ce44b28b3621
Kumatamo-AI <atilla.istami@utexas.edu>
2023-04-09 22:07:27 -0500

    sample tile setup

--- Assets/Scripts/Tile_Script.cs
+++ Assets/Scripts/Tile_Script.cs
@@ -2,22 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Tile_Script : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    public Tile tile;
-    public TextMeshProUGUI title;
+    private Tile tile;
+
+    public GameObject japaneseTitle;
+    public GameObject translatedTitle;
+    public GameObject levelOne;
+    public GameObject levelTwo;
+    public GameObject rulesText;
+    public GameObject resources;
+    public GameObject tileImage;
+    public GameObject resourceImage;
+
     
     void Start()
     {
+        // invoke TSV reader
         CSVReader test = gameObject.AddComponent<CSVReader>() as CSVReader;
         test.ReadCSV();
-        tile = test.queryTile(2);
-        title = gameObject.GetComponent<TextMeshProUGUI>();
-        title.text = tile.transText[1];
-        Debug.Log("This is the thing" + tile.transText[1]);
+        tile = test.queryTile(2); // get the tile loaded
+        TextMeshProUGUI jpTitle = japaneseTitle.GetComponent<TextMeshProUGUI>();
+        jpTitle.text = tile.origText[1];
+
+        TextMeshProUGUI enTitle = translatedTitle.GetComponent<TextMeshProUGUI>();
+        enTitle.text = tile.transText[1];
+
+        TextMeshProUGUI levelOneTxt = levelOne.GetComponent<TextMeshProUGUI>();
+        levelOneTxt.text = tile.histNotes[2];
+
+        TextMeshProUGUI levelTwoTxt = levelTwo.GetComponent<TextMeshProUGUI>();
+        levelTwoTxt.text = tile.histNotes[3];
+
+        // For images it seems like we'd have to get a variable of every image
+        // then load the correct one
+
+
+        
         
     }
 
