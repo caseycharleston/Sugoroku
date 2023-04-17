87664da67bcef727dc6eb8b0fbd881be73f0ba29
caseycharleston <caseycharleston@gmail.com>
2023-04-16 21:56:48 -0500

    Remove scenes we dont need, imported Kevin's stuff

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,94 +1,46 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Dice : MonoBehaviour {
 
-    private static AudioSource dice_land, dice_shake, kabuki1, kabuki2, kabuki3, kabuki_full;
-    public GameObject real_dice, fast_dice;
     private Sprite[] diceSides;
-    private Image rend;
-    public static bool coroutineAllowed = true;
-    public GameObject dramatic_camera;
-
-    //used for debug, force a seed of dice rolls (includes fast travel dice rolls if fast travel is not turned off)
-    // private int[] debug_rolls = {2, 32, 32, 32, 1, 1, 32, 1, 1};
-    // private int debug_index = 0;
+    private SpriteRenderer rend;
+    private int whosTurn = 1;
+    private bool coroutineAllowed = true;
 
 	// Use this for initialization
-	private void Start() {
-        dice_land = GameObject.Find("DiceLand").GetComponent<AudioSource>();
-        dice_shake = GameObject.Find("DiceShake").GetComponent<AudioSource>();
-        kabuki1 = GameObject.Find("KabukiP1").GetComponent<AudioSource>();
-        kabuki2 = GameObject.Find("KabukiP2").GetComponent<AudioSource>();
-        kabuki3 = GameObject.Find("KabukiP3").GetComponent<AudioSource>();
-        kabuki_full = GameObject.Find("KabukiFull").GetComponent<AudioSource>();
-
-        rend = GetComponent<Image>();
+	private void Start () {
+        rend = GetComponent<SpriteRenderer>();
         diceSides = Resources.LoadAll<Sprite>("DiceSides/");
         rend.sprite = diceSides[5];
 	}
 
-    private void Update() {
-        if (Input.GetKeyDown("space")) {
-            if (coroutineAllowed) {
-                StartCoroutine("RollTheDice");
-            }
-        }
-    }
-
-    private void OnMouseDown()
+    private void OnMouseDown() //For the mouse action on the  game board
     {
-        if (coroutineAllowed) {
+        if (!GameControl.gameOver && coroutineAllowed)
             StartCoroutine("RollTheDice");
-        }
     }
 
     private IEnumerator RollTheDice()
     {
         coroutineAllowed = false;
         int randomDiceSide = 0;
-        if (GameControl.fast_travel) {
-            GameControl.brain.m_DefaultBlend.m_Time = 5; // 0 Time equals a cut
-            dramatic_camera.SetActive(true);
-            dice_shake.loop = true;
-            dice_shake.Play();
-            kabuki1.Play();
-            rend = fast_dice.GetComponent<Image>();
-            // kabuki_full.Play();
-            for (int i = 0; i <= 95; i++) {
-                randomDiceSide = Random.Range(0, 6);
-                rend.sprite = diceSides[randomDiceSide];
-                yield return new WaitForSeconds(0.05f);
-            }
-            dice_shake.loop = false;
-            dice_shake.Stop();
-            kabuki2.Play();
-        } else {
-            dice_shake.Play();
-            rend = real_dice.GetComponent<Image>();
-            for (int i = 0; i <= 25; i++) {
-                randomDiceSide = Random.Range(0, 6);
-                rend.sprite = diceSides[randomDiceSide];
-                yield return new WaitForSeconds(0.05f);
-            }
+        for (int i = 0; i <= 20; i++)
+        {
+            randomDiceSide = Random.Range(1, 7);
+            rend.sprite = diceSides[randomDiceSide - 1];
+            yield return new WaitForSeconds(0.05f);
         }
-        // Debug.Log("Rolled: " + (randomDiceSide + 1));
-        GameControl.diceSideThrown = randomDiceSide + 1;
-        // GameControl.diceSideThrown = 32; //DEBUG: force the dice roll value
-        // GameControl.diceSideThrown = debug_rolls[debug_index]; debug_index++; //  DEBUG force a dice roll seed
-        dice_land.Play();
-        yield return new WaitForSeconds(1f);
-     
-        if (GameControl.fast_travel) {
-            GameControl.brain.m_DefaultBlend.m_Time = 0; // 0 Time equals a cut
-            dramatic_camera.SetActive(false);
-            yield return new WaitForSeconds(1f);
-            kabuki3.Play();
-            GameControl.brain.m_DefaultBlend.m_Time = GameControl.zoom_speed; // 0 Time equals a cut
+
+        GameControl.diceSideThrown = randomDiceSide;
+        if (whosTurn == 1)
+        {
+            GameControl.MovePlayer(1);
+        } else if (whosTurn == -1)
+        {
+            GameControl.MovePlayer(2);
         }
-        GameControl.MovePlayer();
+        whosTurn *= -1;
         coroutineAllowed = true;
     }
 }
-
--- Assets/Scripts/FollowThePath.cs
+++ Assets/Scripts/FollowThePath.cs
@@ -1,38 +1,20 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 
 public class FollowThePath : MonoBehaviour {
 
-    private static AudioSource move_sfx;
-    public int player_id;
-    public Transform[] tl_waypoints;
-    public Transform[] tr_waypoints;
-    public Transform[] bl_waypoints;
-    public Transform[] br_waypoints;
     public Transform[] waypoints;
-    [HideInInspector] public List<Transform[]> wp = new List<Transform[]>();
 
-    [SerializeField] private float moveSpeed = 1f;
-    [HideInInspector] public int waypointIndex = 0;
-    public bool moveAllowed = false;
-    const float pause_move_time = .2f;
+    [SerializeField]
+    private float moveSpeed = 2f; //for the 16 code for the value of the speed
+
+    [HideInInspector] //need to be reviewed and fixed
+    public int waypointIndex = 0; // waypoint setting value
+
+    public bool moveAllowed = false; 
 
 	// Use this for initialization
 	private void Start () {
-        move_sfx = GameObject.Find("PlayerMove").GetComponent<AudioSource>();
-        moveAllowed = false;
-        wp.Clear();
-        wp.Add(tl_waypoints); wp.Add(tr_waypoints); wp.Add(bl_waypoints); wp.Add(br_waypoints);
-        if (!GameControl.debug) {
-            if (player_id <= GameControl.num_players) {
-                transform.position = wp[player_id - 1][waypointIndex].transform.position;
-            }
-        } else {
-            transform.position = wp[player_id - 1][waypointIndex].transform.position;
-        }
-        waypointIndex = 1;
+        transform.position = waypoints[waypointIndex].transform.position;
 	}
 	
 	// Update is called once per frame
@@ -41,32 +23,18 @@ public class FollowThePath : MonoBehaviour {
             Move();
 	}
 
-    private void Move() {
-        if (waypointIndex < tl_waypoints.Length) {
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
             transform.position = Vector2.MoveTowards(transform.position,
-            wp[GameControl.square_pos - 1][waypointIndex].transform.position, moveSpeed * Time.deltaTime);
-            if (transform.position ==  wp[GameControl.square_pos - 1][waypointIndex].transform.position) {
-                if (GetComponent<PlayerInfo>().reverse_path) {
-                    waypointIndex -= 1;
-                } else {
-                    waypointIndex += 1;
-                }
-                moveAllowed = false;
-                move_sfx.Play();
-                Invoke("delay_move", pause_move_time);
+            waypoints[waypointIndex].transform.position,
+            moveSpeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
             }
         }
     }
-
-     //Allow player to move again.
-    private void delay_move() {
-            if (!GameControl.stop_move) {
-                moveAllowed = true;
-            }
-            int offset = GetComponent<PlayerInfo>().reverse_path ? 1 : -1;
-            if ((waypointIndex + offset) == GameControl.new_pos) {
-                GameControl.finish_move = true;
-            } 
-    }
-
 }
