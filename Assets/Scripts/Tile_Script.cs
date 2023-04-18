892d3c860014d997837008f4946ba37e64b05006
Kumatamo-AI <atilla.istami@utexas.edu>
2023-04-18 15:58:51 -0500

    add audio support

--- Assets/Scripts/Tile_Script.cs
+++ Assets/Scripts/Tile_Script.cs
@@ -30,8 +30,14 @@ public class Tile_Script : MonoBehaviour
         // invoke TSV reader
         CSVReader test = gameObject.AddComponent<CSVReader>() as CSVReader;
         test.ReadCSV();
-        Debug.Log("querying for " +GameControl.lastRollWayPoint);
+        Debug.Log("querying for " + GameControl.lastRollWayPoint);
         tile = test.queryTile(GameControl.lastRollWayPoint); // get the tile loaded
+
+        // play music associated with the tile
+        AudioSource music = gameObject.AddComponent<AudioSource>(); // add audio object
+        AudioClip clip = (AudioClip) (Resources.Load("audioTile" + GameControl.lastRollWayPoint)); // get the AudioClip
+        music.PlayOneShot(clip); // play music
+
         TextMeshProUGUI jpTitle = japaneseTitle.GetComponent<TextMeshProUGUI>();
         jpTitle.text = tile.origText[0];
 
