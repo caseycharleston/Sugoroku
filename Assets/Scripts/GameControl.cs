0debf001072b9a1005d18184698417beb3595c4a
Kumatamo-AI <atilla.istami@utexas.edu>
2023-04-18 19:27:29 -0500

    camera put in but no worky

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -16,6 +16,28 @@ public class GameControl : MonoBehaviour {
     public bool[] visited = new bool[60];
 
     public static bool gameOver = false;
+    public Transform[] waypoints;
+
+    // angle to zoom for camera movement
+    private int[] direction = 
+        {
+            0, 0, 0, 0, 0, // tiles 1-5
+            -90, -90, -90, -90, -90, // tiles 6-10
+            180, 180, 180, 180, 180, 180, 180, // tiles 11-17
+            90, 90, 90, 90, // tiles 18-21
+            0, 0, 0, 0, 0, 0, // tiles 22-27
+            -90, -90, -90, // tiles 28-30
+            180, 180, 180, 180, 180, 180, 180, // tiles 31-37
+            90, 90, 90, // tiles 38-40
+            0, 0, 0, 0, 0, 0, // tiles 41-46
+            -90, -90, // tiles 47-48
+            180, 180, 180, 180, 180, // tiles 49-53
+            90, 90, 90, 90, 90, 90, // tiles 54-59
+            0 // tile 60
+        };
+
+        private float speed = 1.25f;
+        private float speen = 20.0f;
 
     // Use this for initialization
     void Start () {
@@ -45,12 +67,18 @@ public class GameControl : MonoBehaviour {
             player2MoveText.gameObject.SetActive(true);
             player1StartWaypoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
             lastRollWayPoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
-            if (!visited[player1StartWaypoint]) {
-                visited[player1StartWaypoint] = true;
-                Initiate.Fade("Base_Tile", Color.black, 1f);
-            } else {
-                // ask player if they wanna see it again
+            Vector3 newPos = new Vector3(waypoints[lastRollWayPoint].transform.position.x, waypoints[lastRollWayPoint].transform.position.y, Camera.main.transform.position.z);
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newPos, speed * Time.fixedDeltaTime); // move camera to center at waypoint
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(0.0f, 0.0f, direction[0]), speed * Time.fixedDeltaTime); // rotate camera
+            while (Camera.main.fieldOfView >= 10.0f){
+                Camera.main.fieldOfView -= speen * Time.deltaTime; // zoom in 
             }
+            // if (!visited[player1StartWaypoint]) {
+            //     visited[player1StartWaypoint] = true;
+            //     Initiate.Fade("Base_Tile", Color.black, 1f);
+            // } else {
+            //     // ask player if they wanna see it again
+            // }
         }
 
         if (player2.GetComponent<FollowThePath>().waypointIndex > player2StartWaypoint + diceSideThrown)
